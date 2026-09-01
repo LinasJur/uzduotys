5649cb7cc46de185a64461731aecbb357cc51f03
Linas Jurgelevičius <jurgeleviciuslinas@gmail.com>
2026-09-01 08:14:16 +0300

    + vartotojas gali koreguoti savo duomenis

--- mysite/uzduotys/urls.py
+++ mysite/uzduotys/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.TaskListView.as_view(), name='tasks'),
+    path('', views.index, name='index'),
     path('tasks/<int:pk>/', views.TaskDetailView.as_view(), name='task'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('usertasks/', views.UserTaskListView.as_view(), name='user_tasks'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
+
 ]
--- mysite/uzduotys/views.py
+++ mysite/uzduotys/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import Task
+from .forms import  UserChangeForm, CustomUserCreateForm
 
 class TaskListView(generic.ListView):
     model = Task
@@ -21,11 +22,21 @@ class UserTaskListView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'tasks'
 
     def get_queryset(self):
-        return Task.objects.filter(user=self.request.user)
+        return Task.objects.filter(author=self.request.user)
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreateForm
     template_name = "signup.html"
     success_url = reverse_lazy("login")
 
-    
+def index(request):
+
+    return render(request, template_name='index.html')
+
+class ProfileUpdateView(LoginRequiredMixin, generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = "profile.html"
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
